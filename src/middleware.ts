06b35b331abc067aa66e6cee151efd00ba6f8def
Your Name <your.email@example.com>
2025-01-03 01:07:15 +0530

    middle_updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,21 @@
-import { NextResponse } from 'next/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// Middleware function that does nothing and allows all requests to pass through
-export function middleware() {
-  return NextResponse.next()
-}
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forum(.*)'])
 
-// Configure middleware to apply to no routes by providing an empty matcher array
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth()
+
+  if (!userId && isProtectedRoute(req)) {
+    // Add custom logic to run before redirecting
+
+    return redirectToSignIn()
+  }
+})
+
 export const config = {
-  matcher: [], // No routes matched, so middleware is effectively disabled
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    
+  ],
 }
