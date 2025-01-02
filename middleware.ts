44fe94553542c2c80e45af5f034405b0987f78e7
Your Name <your.email@example.com>
2025-01-03 01:43:36 +0530

    idk

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forum(.*)'])
+const isWebhookRoute = createRouteMatcher(['/api/webhook(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
+  // If the request is for a webhook route, skip the authentication check
+  if (isWebhookRoute(req)) {
+    return;
+  }
+
   const { userId, redirectToSignIn } = await auth()
 
   if (!userId && isProtectedRoute(req)) {
     // Add custom logic to run before redirecting
-
     return redirectToSignIn()
   }
 })
@@ -16,6 +21,5 @@ export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    
   ],
 }
